4ab19a12551b570db5835f58e899d7912e43f878
louisalemar <louisalemar@gmail.com>
2019-06-29 21:36:18 -0400

    set up webpack dev server

---
+++ volumes/front_end/webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+	  app: './src/index.js'
+	},
+	devServer: {
+	  contentBase: path.join(__dirname, 'dist'),
+	  host: '0.0.0.0',
+	  port: 3000,
+	  historyApiFallback: true,
+	},
+	output: {
+	  filename: '[name].bundle.js',
+	  chunkFilename: '[name].bundle.js',
+	  path: path.resolve(__dirname, 'dist'),
+	  publicPath: '/'
+	},
+	plugins: [
+		new HtmlWebpackPlugin()
+	]
+}
