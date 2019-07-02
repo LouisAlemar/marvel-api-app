4e9d8114eeed9e78df154323a1d7327347a9b38f
louisalemar <louisalemar@gmail.com>
2019-07-01 21:24:53 -0400

    feat: set up redux store

--- volumes/front_end/src/index.js
+++ volumes/front_end/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import axios from 'axios';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
+console.log(store.getState())
 
 render(<App />, document.getElementById(APP_MOUNT_ID));
