b5b451d58515821023d36faeca6b9de736d637ed
louisalemar <louisalemar@gmail.com>
2019-07-06 20:13:54 -0400

    feat: add character modal

--- volumes/front_end/src/components/CharacterItem/index.js
+++ volumes/front_end/src/components/CharacterItem/index.js
@@ -1,20 +1,35 @@
 import React, {Component} from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import CharacterModal from '../CharacterModal';
 
 class CharacterItem extends Component{
 	constructor(props){
 		super(props)
+
+		this.state = {
+			modal: false
+		}
+
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle(){
+		this.setState(prevState => ({
+			modal: !prevState.modal
+		}))
 	}
 
 	render(){
-		console.log(this.props.character)
+		// console.log(this.props.character)
 		return(
-			<div className="character-item">
+			<div className="character-item" onClick={this.toggle}>
 				<div className="name-container">
 					<h3 className="character-name">{this.props.character.name}</h3>
 				</div>
 				<div className="image-container">
 					<div className="character-pic" style={{backgroundImage: `url(${this.props.character.image.url})`}}></div>
 				</div>
+				<CharacterModal state={this.state} character={this.props.character} toggle={this.toggle} />
 			</div>
 		)
 	}
